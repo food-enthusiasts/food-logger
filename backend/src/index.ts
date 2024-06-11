8bbdc4a12532bde62a85b9af5f63de1fde6ae441
alex <alexanderyi933@gmail.com>
2024-06-11 17:12:56 -0400

    add 404 handler to express api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,4 +6,8 @@ app.get("/", (_, res) => {
   res.send("hello");
 });
 
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
 app.listen(5050, () => console.log("server running"));
