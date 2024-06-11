a0349de97f70dfaa75a995724c38d6bf820bd1b2
alex <alexanderyi933@gmail.com>
2024-06-11 17:02:22 -0400

    install and configure tailwind in the remix app based on tailwind docs:
    https://tailwindcss.com/docs/guides/remix

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -11,6 +11,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <div className="bg-slate-500">Hello remix</div>
       <ul>
         <li>
           <a
