bf998e365efc6eff1483e6df3de00a269346caa1
alex <alexanderyi933@gmail.com>
2024-06-21 15:00:35 -0400

    set up express to parse json payloads on req.body

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
 
 const app = express();
+// tested with CURL, if I don't specify content-type as json, seems
+// like CURL defaults to x-www-form-urlencoded content type for POST
+// need to also parse x-www-form-urlencoded request bodies? when would those get set?
+// sent by an html form with POST set as the method?
+app.use(express.json());
 
 app.get("/", (_, res) => {
   res.send("hello");
+});
+
+app.post("/", (req, res) => {
+  console.log("req headers", JSON.stringify(req.headers));
+  console.log("have req.body?", req.body);
+  res.send("posted \n");
 });
 
 app.use((req, res, next) => {
