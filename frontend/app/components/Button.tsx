56a987b5b08a40bf757bd5d5f9b798e9211660b4
alex <alexanderyi933@gmail.com>
2024-06-27 15:00:24 -0400

    adds a button component to the front end

---
+++ frontend/app/components/Button.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+// using Kyle Shevlin's blog Button implementation as inspiration aka reading it and trying to understand it
+
+interface ButtonProps {
+  children: ReactNode;
+  className?: string;
+  onClick?: () => void;
+  type?: HTMLButtonElement["type"];
+  disabled?: boolean;
+}
+
+export function Button({
+  children,
+  className = "",
+  onClick,
+  type = "button",
+  disabled = false,
+}: ButtonProps) {
+  return (
+    <button
+      className={`px-4 py-2 bg-slate-200 rounded ${className}`}
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
