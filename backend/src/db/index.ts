26d79ab80c6a001f2730e0cb0584c5a479d39a65
alex <alexanderyi933@gmail.com>
2024-07-12 16:49:05 -0400

    creates seed file and verifies that it inserts data into the db, also verifies that migrations to drop recipes table then recreate it were required

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,9 +2,17 @@
 // https://github.com/WiseLibs/better-sqlite3
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const sqlite = new Database("foodlogger-dev.db");
+// reference for replicating __dirname functionality in esm (at least I think it's because we're using esm)
+// https://iamwebwiz.medium.com/how-to-fix-dirname-is-not-defined-in-es-module-scope-34d94a86694d
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sqlite = new Database(path.resolve(__dirname, "./foodlogger-dev.db"));
 sqlite.pragma("journal_mode = WAL");
+sqlite.pragma("foreign_keys = ON");
 
 // following drizzle docs for setup
 // https://orm.drizzle.team/docs/get-started-sqlite#better-sqlite3
