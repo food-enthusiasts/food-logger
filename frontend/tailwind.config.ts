af8c1c0715e2177eb5712bc35d31074dbb5d9b79
alex <alexanderyi933@gmail.com>
2024-08-11 18:47:56 -0400

    add aliases for tailwind colors to reference in styles

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,9 +1,19 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.orange,
+        secondary: colors.yellow,
+        warn: colors.indigo,
+        error: colors.red,
+        buttonPrimary: colors.orange[300],
+        // buttonSecondary: colors.yellow[400],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
