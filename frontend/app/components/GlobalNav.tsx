eb8922a571c32d91ddf7165cc6e280e90d43a865
alex <alexanderyi933@gmail.com>
2024-08-12 01:05:21 -0400

    wip GlobalNav component with non-functional hamburger on screens smaller than large, and full menu on large screens

---
+++ frontend/app/components/GlobalNav.tsx
@@ -0,0 +1,52 @@
+import { Link } from "@remix-run/react";
+// building off tailwind ui "With stacked flyout menu" header design
+// https://tailwindui.com/components/marketing/elements/headers#component-27e5f71ced91b88e3f6b59ca69033a83
+export function GlobalNav() {
+  return (
+    <header className="p-6 shadow-sm ">
+      <nav className="flex justify-between">
+        <div>
+          {/* pretend this div is a logo */}
+          <Link to="/">
+            <div className="w-20 h-8 bg-slate-500"></div>
+          </Link>
+        </div>
+        {/* mobile, tablet hamburger nav */}
+        <HamburgerMenu />
+        {/* desktop nav items */}
+        <div className="hidden lg:flex gap-20">
+          <Link to="/register">Register</Link>
+          <Link to="/login">Log In</Link>
+          <div>adas</div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+// implementation taken from https://tailwindui.com/components/marketing/elements/headers#component-27e5f71ced91b88e3f6b59ca69033a83
+function HamburgerMenu() {
+  return (
+    <div className="flex lg:hidden">
+      <button
+        type="button"
+        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+      >
+        <span className="sr-only">Open main menu</span>
+        <svg
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
