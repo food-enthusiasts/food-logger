40840b077cc50380533a57384a626ce8f845e1eb
alex <alexanderyi933@gmail.com>
2024-09-07 17:15:00 -0400

    adds password column to users table

--- app/db/schema.server.ts
+++ app/db/schema.server.ts
@@ -14,6 +14,10 @@ export const users = mysqlTable("users", {
   id: int("id").primaryKey(),
   username: varchar("username", { length: 256 }).notNull().unique(),
   email: varchar("email", { length: 256 }).notNull().unique(),
+  // seems like bcrypt can output hashes that are 60 characters long
+  // so using varchar of length 80 just in case it's actually a slightly
+  // larger length
+  password: varchar("password", { length: 80 }).notNull(),
   // for mysql timestamps with drizzle, referencing this thread from a site that aggregrates messages in the drizzle discord:
   // https://www.answeroverflow.com/m/1113986761606037575
   createdAt: timestamp("created_at").notNull().defaultNow(),
---
+++ drizzle/0001_silent_dreaming_celestial.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `users` ADD `password` varchar(80) NOT NULL;
