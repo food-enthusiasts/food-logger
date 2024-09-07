b902c5f1e144faed9a9d12726740f7bf03619520
alex <alexanderyi933@gmail.com>
2024-09-07 17:07:54 -0400

    add bcrypt as a dependency and add back in db initialization code

---
+++ app/db/index.server.ts
@@ -0,0 +1,15 @@
+// following docs for better-sqlite3 for this setup
+// https://github.com/WiseLibs/better-sqlite3
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+// following drizzle docs for setup
+// https://orm.drizzle.team/docs/get-started-mysql#mysql2
+export const db = drizzle(connection);
