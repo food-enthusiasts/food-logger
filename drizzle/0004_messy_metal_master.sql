9c0b79e7155c4bd0bc1bf93a061803bcf16ce64c
alex <alexanderyi933@gmail.com>
2024-09-10 19:15:59 -0400

    add foreign key to users table id back into recipes table

---
+++ drizzle/0004_messy_metal_master.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `recipes` ADD `user_id` int NOT NULL;--> statement-breakpoint
+ALTER TABLE `recipes` ADD CONSTRAINT `recipes_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE no action ON UPDATE no action;
