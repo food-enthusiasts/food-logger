a4d68af940f1c780929748d9fd0fe46c505ddc38
alex <alexanderyi933@gmail.com>
2024-09-13 13:32:16 -0400

    update vite config to allow remix build:vite to work. Previously was failing because of use of top level await in a server module

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,12 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  // added build.module = "ES2022" to make remix build command complete. Before adding, was failing because I use top level await
+  // referenced this answer on Github: https://github.com/remix-run/remix/issues/7969#issuecomment-1806322036
+  // makes me question my "target" and "module" settings in tsconfig (current set to "target": "ES2022", "module": "ESNext" as of 9/13/2024)
+  // read this SO post for some clarification, but didn't add much:
+  // https://stackoverflow.com/questions/39493003/typescript-compile-options-module-vs-target
+  build: {
+    target: "ES2022",
+  },
 });
