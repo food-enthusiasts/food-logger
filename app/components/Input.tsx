f7dc5f54883de0bbf70f84a93e462186ca1ad310
alex <alexanderyi933@gmail.com>
2024-09-26 21:42:23 -0400

    forgot to pass rest of props through Input component

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -10,6 +10,7 @@ export function Input({ ...props }: InputProps) {
       className={`px-2 py-2 border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm focus:outline-none ${
         props.className ?? ""
       }`}
+      {...props}
     ></input>
   );
 }
