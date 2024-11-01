abbbd051e1cdf49dafb7071d079dbdfb7436f410
alex <alexanderyi933@gmail.com>
2024-11-01 15:52:56 -0400

    fix bug in Input component that caused styles passed in via className to override all default existing classNames that were already part of Input. This was occurring because I passed in props like so:
    <Input className={"existing classes go here" + extraStuff} {...props} />
    
    This made it so any time I passed in a className prop to Input, the className prop would get applied, then immediately overridden by the {...props} spread operation bc props include className, leaving the className to end up  as only "extraStuff" instead of the existing names plus "extraStuff". Fixed this by applying the props spread first, then applying className after so it would not get overridden by the spread

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -7,10 +7,10 @@ interface InputProps extends ComponentProps<"input"> {}
 export function Input({ ...props }: InputProps) {
   return (
     <input
+      {...props}
       className={`px-2 py-2 border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm focus:outline-none ${
         props.className ?? ""
       }`}
-      {...props}
     ></input>
   );
 }
