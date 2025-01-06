dcc938775fb622f030ae30e9b933cc85b6dfbaca
alex <alexanderyi933@gmail.com>
2025-01-06 16:00:31 -0500

    wraps user creation and bulk user creation service methods in db transaction

--- app/repositories/user.server.ts
+++ app/repositories/user.server.ts
@@ -93,17 +93,71 @@ export class UserRepository {
     try {
       const passwordHash = await bcrypt.hash(password, 10);
 
-      const newUserCreationResponse = await this.client
+      const newUserId = await this.client.transaction(async (transaction) => {
+        const newUserCreationResponse = await transaction
         .insert(users)
         .values({ username, email, password: passwordHash })
         .$returningId();
 
-      if (newUserCreationResponse.length !== 1)
+        if (newUserCreationResponse.length !== 1) {
+          // calling transaction.rollback() is supposed to throw an error that rolls back the transaction
+          // so I guess I can catch the error and then throw my own error?
+          // transaction.rollback();
+
+          // however, seems like transaction.rollback() itself doesn't do anything special and just throws an error per
+          // this SO post (which also references the mysql2 driver drizzle source code)
+          // https://stackoverflow.com/questions/78784936/does-drizzle-orm-auto-rollbacks-when-there-is-an-exception-or-do-i-need-to-call
+          // so makes me think I can just throw my own error here and it will achieve the same thing
+          // also reference to drizzle GH issue: https://github.com/drizzle-team/drizzle-orm/issues/1723#issuecomment-1946836303
         throw new UserRepoValidationError(
           "Created user response either returned 0 entries or more than 1 entry"
         );
+        }
 
       return newUserCreationResponse[0].id;
+      });
+
+      return newUserId;
+    } catch (err) {
+      console.error("Error in user repo: createUser", err);
+      throw err;
+    }
+  }
+
+  // using Parameters type to extract params from createUser method instead of writing the same type twice
+  // just duplicating the type def seems more readable, but may as well test this out!
+  // Parameters returns a type tuple of the params, so I'm accessing the type of the first param defined in createUsers
+  async bulkCreateUsers(newUsers: Parameters<typeof this.createUser>[0][]) {
+    try {
+      // typing newUsersList by indexing into the newUser with number to extract the type of an individual user
+      const newUsersList: Array<(typeof newUsers)[number]> = [];
+
+      // replacing the plaintext password with hashed version for every user
+      for (const newUser of newUsers) {
+        newUsersList.push({
+          ...newUser,
+          password: await bcrypt.hash(newUser.password, 10),
+        });
+      }
+
+      const newUsersIdList = await this.client.transaction(
+        async (transaction) => {
+          const newUsersCreationResponse = await transaction
+            .insert(users)
+            .values(newUsersList)
+            .$returningId();
+
+          if (newUsersCreationResponse.length !== newUsers.length) {
+            throw new UserRepoValidationError(
+              `Number of users created does not match number of users given. Was given ${newUsers.length} users to create and attempted to create ${newUsersCreationResponse.length} in the database`
+            );
+          }
+
+          return newUsersCreationResponse;
+        }
+      );
+
+      return newUsersIdList.map((userIdObj) => userIdObj.id);
     } catch (err) {
       console.error("Error in user repo: createUser", err);
       throw err;
